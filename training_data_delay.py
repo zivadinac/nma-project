9ca374dae0a38a26db10cbf94324226d5cc1240e
tamaradelaguila <tamarama3@gmail.com>
2020-07-28 19:19:58 +0200

    many shuffled repetitions for (1) different time-windows (2) shuffled data

--- training_data_delay.py
+++ training_data_delay.py
@@ -144,105 +144,123 @@ neural_data = dat['sresp']
 run_data = dat['run']
 run_onset, run_speed = movement.detect_movement_onset(run_data)
 
-#%% LOGISTIC REGRESSION ON NEURAL DATA - train model
+#%% GLM in neural data (frate)
+
 # SET PARAMETERS
 C = np.logspace(-4, 0, 20)
-det_window = 1
 neuron_num = 4000
-pca_com = 13
+pca_com = 200
 
-#use only one of this two lines!
-feat = extract_features(neural_data, neurons_idx=np.random.randint(0, len(neural_data), neuron_num))
-# feat = extract_features(neural_data, pca_comp_num =pca_com)
+delay = 0
+det_window = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] #np.array(range(1:10))
+n_shuffle = 100
 
-delay_bins = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20]
-
-test_acc = np.zeros([len(delay_bins), 2])
-train_acc = np.zeros([len(delay_bins), 2])
+test_acc = np.zeros([2, len(det_window), n_shuffle])
+train_acc = np.zeros([2, len(det_window), n_shuffle])
 
 
-for delay in range(0,len(delay_bins)):
-    X_train, y_train, X_test, y_test = prepare_data_delay(feat, run_onset, det_window, delay, 0.2)
+for shuffle in range(n_shuffle):
     
-    decoder = LogisticRegressionCV(Cs = C, penalty="l1", solver='liblinear')
-    decoder.fit(X_train, y_train)
-    acc_test = decoder.score(X_test,y_test)
-    acc_train = decoder.score(X_train,y_train)
-    test_acc[delay, 0] = acc_test
-    train_acc[delay,0] = acc_train
+    feat = extract_features(neural_data, neurons_idx=np.random.randint(0, len(neural_data), neuron_num))
     
-    decoder = LogisticRegressionCV(Cs = C, penalty="l2", solver='liblinear')
-    decoder.fit(X_train, y_train)
-    acc_test = decoder.score(X_test,y_test)
-    acc_train = decoder.score(X_train,y_train)
-    test_acc[delay, 1] = acc_test
-    train_acc[delay,1] = acc_train
+    for w_idx, window in enumerate(det_window):
+        X_train, y_train, X_test, y_test = prepare_data_delay(feat, run_onset, window, delay, 0.2)
+        
+        decoder = LogisticRegressionCV(Cs = C, penalty="l1", solver='liblinear')
+        decoder.fit(X_train, y_train)
+        acc_test = decoder.score(X_test,y_test)
+        acc_train = decoder.score(X_train,y_train)
+        test_acc[0, w_idx, shuffle] = acc_test
+        train_acc[0, w_idx, shuffle]= acc_train
+        
+        decoder = LogisticRegressionCV(Cs = C, penalty="l2", solver='liblinear')
+        decoder.fit(X_train, y_train)
+        acc_test = decoder.score(X_test,y_test)
+        acc_train = decoder.score(X_train,y_train)
+        test_acc[1, w_idx,shuffle] = acc_test
+        train_acc[1, w_idx,shuffle] = acc_train
 
 # PLOT
+
 fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(16, 6))
                       
-ax1.plot(delay_bins, test_acc[:,0].flatten(), 'b.-', label='test acc')
-ax1.plot(delay_bins, train_acc[:, 0].flatten(), 'r.-', label='train acc')
-ax1.set(title = "l1 regularization (f rate)", xlabel= "delay(timebins)")
+ax1.plot(det_window, test_acc[0,:,:].mean(axis=1), 'b.-', label='test')
+ax1.plot(det_window, test_acc[0,:,:], 'b.-', alpha=.1)
+ax1.plot(det_window, train_acc[0,:,:].mean(axis=1), 'r.-', label='train')
+ax1.plot(det_window, train_acc[0,:,:], 'r.-',alpha=.1)
+ax1.set(title = "l1 regularization (f rate)", xlabel= "window size")
 ax1.legend()
-                
-ax2.plot(delay_bins, test_acc[:,1], 'b.-', label='test acc')
-ax2.plot(delay_bins, train_acc[:, 1], 'r.-', label='train acc')
-ax2.set(title= "l2 regularization (f rate)", xlabel= "delay(timebins)")
+     
+ax2.plot(det_window, test_acc[1,:,:].mean(axis=1), 'b.-', label='test')
+ax2.plot(det_window, test_acc[1,:,:], 'b.-', alpha=.1)
+ax2.plot(det_window, train_acc[1,:,:].mean(axis=1), 'r.-', label='train')
+ax2.plot(det_window, train_acc[1,:,:], 'r.-', alpha=.1)
+ax2.set(title= "l2 regularization (f rate)", xlabel= "window size", ylabel= "accuracy level")
 ax2.legend()
-                  
-fig.suptitle (f'frate-GLM on {neuron_num} neurons with a time window of {det_window} timebins')
+
+fig.suptitle (f'frate-GLM on {neuron_num} neurons shuffled {n_shuffle} times')
 
 
-#%% GLM ON MESSY NEURAL DATA
-neural_data_messy = neural_data[:,np.random.permutation(len(neural_data[0,:]))]
+#%% GLM ON MESSY DATA
 
 # SET PARAMETERS
 C = np.logspace(-4, 0, 20)
 det_window =5
-neuron_num = 2000
+delay = 0
+neuron_num = 4000
 pca_com = 200
 
-#use only one of this two lines!
 
-#use only one of this two lines!
-feat = extract_features(neural_data_messy, neurons_idx=np.random.randint(0, len(neural_data_messy), neuron_num))
-#feat = extract_features(neural_data, pca_comp_num =pca_com)
+delay_bins = [0, 1, 2]#, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25]
+n_shuffle = 2
 
-delay_bins = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20]
+test_acc = np.zeros([2, len(delay_bins), n_shuffle])
+train_acc = np.zeros([2, len(delay_bins), n_shuffle])
 
-test_acc = np.zeros([len(delay_bins), 2])
-train_acc = np.zeros([len(delay_bins), 2])
 
-for delay in range(0,len(delay_bins)):
-    X_train, y_train, X_test, y_test = prepare_data_delay(feat, run_onset, det_window, delay, 0.2)
+for shuffle in range(n_shuffle):
     
-    decoder = LogisticRegressionCV(Cs = C, penalty="l1", solver='liblinear')
-    decoder.fit(X_train, y_train)
-    acc_test = decoder.score(X_test,y_test)
-    acc_train = decoder.score(X_train,y_train)
-    test_acc[delay, 0] = acc_test
-    train_acc[delay,0] = acc_train
+    neural_data_messy = neural_data[:,np.random.permutation(len(neural_data[0,:]))]
     
-    decoder = LogisticRegressionCV(Cs = C, penalty="l2", solver='liblinear')
-    decoder.fit(X_train, y_train)
-    acc_test = decoder.score(X_test,y_test)
-    acc_train = decoder.score(X_train,y_train)
-    test_acc[delay, 1] = acc_test
-    train_acc[delay,1] = acc_train
+    #use only one of this two lines!
+    
+    #use only one of this two lines!
+    feat = extract_features(neural_data_messy, neurons_idx=np.random.randint(0, len(neural_data_messy), neuron_num))
+    #feat = extract_features(neural_data, pca_comp_num =pca_com)
+    
+    
+    for delay in delay_bins:
+        X_train, y_train, X_test, y_test = prepare_data_delay(feat, run_onset, det_window, delay, 0.2)
+        
+        decoder = LogisticRegressionCV(Cs = C, penalty="l1", solver='liblinear')
+        decoder.fit(X_train, y_train)
+        acc_test = decoder.score(X_test,y_test)
+        acc_train = decoder.score(X_train,y_train)
+        test_acc[0, delay, shuffle] = acc_test
+        train_acc[0, delay, shuffle] = acc_train
+        
+        decoder = LogisticRegressionCV(Cs = C, penalty="l2", solver='liblinear')
+        decoder.fit(X_train, y_train)
+        acc_test = decoder.score(X_test,y_test)
+        acc_train = decoder.score(X_train,y_train)
+        test_acc[1, delay, shuffle] = acc_test
+        train_acc[1, delay, shuffle] = acc_train
 
 # PLOT
 fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(16, 6))
                       
-ax1.plot(delay_bins, test_acc[:,0].flatten(), 'b.-', label='test acc')
-ax1.plot(delay_bins, train_acc[:, 0].flatten(), 'r.-', label='train acc')
-ax1.set(title = "l1 regularization (f rate)", xlabel= "delay(timebins)")
+ax1.plot(delay_bins, test_acc[0,:,:].mean(axis=1), 'b.-', label='test acc')
+ax1.plot(delay_bins, test_acc[0,:,:], 'b.-', alpha= .1 )
+ax1.plot(delay_bins, train_acc[0,:,:].mean(axis=1), 'r.-', label='train acc')
+ax1.plot(delay_bins, train_acc[0,:,:], 'r.-', alpha= .1 )
+ax1.set(title = "l1 regularization (f rate)", xlabel= "delay(timebins)", ylabel= "accuracy levels")
 ax1.legend()
                 
-ax2.plot(delay_bins, test_acc[:,1], 'b.-', label='test acc')
-ax2.plot(delay_bins, train_acc[:, 1], 'r.-', label='train acc')
-ax2.set(title= "l2 regularization (f rate)", xlabel= "delay(timebins)")
+ax2.plot(delay_bins, test_acc[1,:,:].mean(axis=1), 'b.-', label='test acc')
+ax2.plot(delay_bins, test_acc[1,:,:], 'b.-', alpha=.1)
+ax2.plot(delay_bins, train_acc[1,:,:].mean(axis=1), 'r.-', label='train acc')
+ax2.plot(delay_bins, train_acc[1,:,:], 'r.-', alpha= .1 )
+ax2.set(title= "l2 regularization (f rate)", xlabel= "delay(timebins)", ylabel= "accuracy levels")
 ax2.legend()
 
-fig.suptitle (f'MESSY DATA: frate-GLM on {neuron_num} neurons with a time window of {det_window} timebins')
-
+fig.suptitle (f'MESSY DATA - {n_shuffle} shuffles: frate-GLM on {neuron_num} neurons with a time window of {det_window} timebins')
